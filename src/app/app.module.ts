f45af31974c8429b77103846d254209bd2beb13b
ramchandravgr <motaparthykavya@gmail.com>
2020-08-11 08:05:06 +0530

    Post order update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatRadioModule} from '@angular/material/radio';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatSliderModule} from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,14 +13,17 @@ import {MatButtonModule} from '@angular/material/button';
 import{FlexLayoutModule} from '@angular/flex-layout';
 import { MainComponent } from './main/main.component';
 import {MatMenuModule} from '@angular/material/menu';
+import { PostOrderComponent } from './post-order/post-order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    PostOrderComponent
   ],
   imports: [
     BrowserModule,
+    MatRadioModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
@@ -26,11 +31,12 @@ import {MatMenuModule} from '@angular/material/menu';
     MatListModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatMenuModule
-    
+    MatMenuModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent,
-  MainComponent]
+  MainComponent,
+  PostOrderComponent]
 })
 export class AppModule { }
---
+++ src/app/post-order/post-order.module.ts
@@ -0,0 +1,14 @@
+import { MatRadioModule } from '@angular/material/radio';
+import {MatSliderModule} from '@angular/material/slider';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatRadioModule,
+    MatSliderModule
+  ]
+})
+export class PostOrderModule { }
