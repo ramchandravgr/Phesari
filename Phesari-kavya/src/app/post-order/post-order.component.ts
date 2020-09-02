ed3f39f6841f1b265f4b1f5a68a9ebbc285450d8
kavya180031110 <motaparthykavya@gmail.com>
2020-09-02 09:11:20 +0530

    new

---
+++ Phesari-kavya/src/app/post-order/post-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-order',
+  templateUrl: './post-order.component.html',
+  styleUrls: ['./post-order.component.css']
+})
+export class PostOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
